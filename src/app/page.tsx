d6771c8e79f073b42fcfdb1a19f7691b8fa6e193
MaryamCoader <maryamqureshimumtazm.a@gmail.com>
2026-01-09 23:27:49 +0500

    ui updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,6 @@ import About from "@/components/About";
 import Skills from "@/components/Skills";
 import HackathonProjects from "@/components/HackathonProjects";
 import Projects from "@/components/Projects";
-import Education from "@/components/Education";
-import Experience from "@/components/Experience";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
